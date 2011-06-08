d9be2612183b5ced0f6ed1ba4b27df76d197dd22
Colin Gemmell <pythonandchips@gmail.com>
2011-06-08 23:02:42 +0100

    changed sudo apt-get base class

--- cookbooks/sudo_user/providers/sudo_apt_get.rb
+++ cookbooks/sudo_user/providers/sudo_apt_get.rb
@@ -6,7 +6,7 @@ require 'chef/provider/package/apt'
 class Chef
   class Provider
     class Package
-      class SudoAptGet < Chef::Provider::Package::Apt
+      class SudoAptGet < Chef::Provider::Package
         def install_package(name, version)
           run_command_with_system_locale(
             :command => "sudo apt-get -y -q #{expand_options(@new_resource.options)} install #{name}=#{version}",
