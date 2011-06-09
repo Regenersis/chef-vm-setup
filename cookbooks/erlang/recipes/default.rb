cf53b09a8acae9acb5b671ccec6c781167d55e89
Colin Gemmell <pythonandchips@gmail.com>
2011-06-09 21:12:07 +0100

    added couchdb recipe added erlang recipe as dependancy for couchdb

--- cookbooks/erlang/recipes/default.rb
+++ cookbooks/erlang/recipes/default.rb
@@ -1,21 +1,27 @@
-#
-# Cookbook Name:: main
+# Cookbook Name:: erlang
 # Recipe:: default
+# Author:: Joe Williams <joe@joetify.com>
 #
-# Copyright 2011, Example Com
+# Copyright 2008-2009, Joe Williams
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-#include_recipe "mysql::server"
-#include_recipe "rabbitmq"
-include_recipe "couchdb"
+case node[:platform]
+when "debian", "ubuntu"
+  erlpkg = node[:erlang][:gui_tools] ? "erlang" : "erlang-nox"
+  package erlpkg
+  package "erlang-dev"
+else
+  package "erlang"
+end
