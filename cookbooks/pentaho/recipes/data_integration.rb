1c484f7696da86360b82e31d75fd4aaab8f2f50b
Colin Gemmell <pythonandchips@gmail.com>
2011-06-13 13:17:51 +0100

    changed from using remote file to wget for pentaho files

--- cookbooks/pentaho/recipes/data_integration.rb
+++ cookbooks/pentaho/recipes/data_integration.rb
@@ -1,15 +1,12 @@
-remote_file "/home/#{node[:user]}/pentaho.tar.gz" do
-  source "http://downloads.sourceforge.net/project/pentaho/Data%20Integration/4.1.0-stable/pdi-ce-4.1.0-stable.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpentaho%2Ffiles%2FData%2520Integration%2F4.1.0-stable%2F&ts=1307919295&use_mirror=kent"
-end
-
 script "install pentaho" do
   interpreter "bash"
   user "root"
   cwd "/home/#{node[:user]}"
   code <<-EOH
-    tar -zxvf pentaho.tar.gz 
+    wget http://downloads.sourceforge.net/project/pentaho/Data%20Integration/4.1.0-stable/pdi-ce-4.1.0-stable.tar.gz
+    tar -zxvf pdi-ce-4.1.0-stable.tar.gz 
     mkdir #{node[:pentaho][:install_dir]}
-    cp pentaho #{node[:pentaho][:install_dir]}/data-integration -R
+    cp data-integration #{node[:pentaho][:install_dir]}/data-integration -R
     #{node[:pentaho][:install_dir]}/data_integration/set-pentaho-env
   EOH
 end
--- cookbooks/pentaho/recipes/report_designer.rb
+++ cookbooks/pentaho/recipes/report_designer.rb
@@ -1,13 +1,10 @@
-remote_file "/home/#{node[:user]}/report-designer.tar.gz" do
-  source "http://downloads.sourceforge.net/project/pentaho/Report%20Designer/3.8.0-stable/prd-ce-3.8.0-stable.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpentaho%2Ffiles%2FReport%2520Designer%2F3.8.0-stable%2F&ts=1307919352&use_mirror=ignum"
-end
-
 script "install pentaho" do
   interpreter "bash"
   user "root"
   cwd "/home/#{node[:user]}"
   code <<-EOH
-    tar -zxvf report-designer.tar.gz
+    wgt http://downloads.sourceforge.net/project/pentaho/Report%20Designer/3.8.0-stable/prd-ce-3.8.0-stable.tar.gz
+    tar -zxvf prd-ce-3.8.0-stable.tar.gz
     cp report-designer #{node[:pentaho][:install_dir]}/report-designer -R
   EOH
 end
