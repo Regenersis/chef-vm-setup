28612dceeaaad34a58e3f086ecdb567699ace8e9
Colin Gemmell <pythonandchips@gmail.com>
2011-06-13 23:11:24 +0100

    sorted compile error

--- cookbooks/dotfiles/recipes/default.rb
+++ cookbooks/dotfiles/recipes/default.rb
@@ -1,11 +1,8 @@
 ["gemrc", "irbrc"].each do |file|
-template "/home/#{node[:user]}/.#{file}" do
-  source "#{file}.erb"
-  owner node[:user]
-  group node[:user]
-  mode "0644"
-  variables(
-    :name => github_details["name"],
-    :email => github_details["email"]
-  )
+  template "/home/#{node[:user]}/.#{file}" do
+    source "#{file}.erb"
+    owner node[:user]
+    group node[:user]
+    mode "0644"
+  end
 end
