5ab3cbf8db4b0a4b52a7d2f0058f3a06bc626ca4
Colin Gemmell <pythonandchips@gmail.com>
2011-06-13 22:46:41 +0100

    parse json

--- cookbooks/github/recipes/default.rb
+++ cookbooks/github/recipes/default.rb
@@ -1,11 +1,12 @@
 require "rest-client"
+require "json"
 
 package "kdiff3"
 
 package "gitk"
 
 template "/home/#{node[:user]}/.gitconfig" do
-  github_details = RestClient.get("https://api.github.com/users/#{node[:github][:username]}")
+  github_details = JSON.parse(RestClient.get("https://api.github.com/users/#{node[:github][:username]}"))
   source "gitconfig.erb"
   owner node[:user]
   group node[:user]
