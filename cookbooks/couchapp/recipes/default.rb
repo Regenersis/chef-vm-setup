fe0f8e6b40f37fc0fcd25abac9e9dc309b2e9249
Colin Gemmell <pythonandchips@gmail.com>
2011-06-16 22:43:26 +0100

    changed couchapp to install from pip

--- cookbooks/couchapp/recipes/default.rb
+++ cookbooks/couchapp/recipes/default.rb
@@ -1,6 +1,11 @@
-apt_repository "couchapp" do
-  uri "ppa:couchapp/couchapp"
-  action :add
+script "install rubymine" do
+  interpreter "bash"
+  user "root"
+  cwd node[:temp_folder]
+  code <<-EOH
+    curl -O http://python-distribute.org/distribute_setup.py
+    sudo python distribute_setup.py
+    easy_install pip
+    sudo pip install couchapp
+  EOH
 end
-
-package "couchapp"
