7040f58b74d3d5bdc4d2b294de2ca960bff4717a
Colin Gemmell <pythonandchips@gmail.com>
2011-06-25 23:00:02 +0100

    added error handling for git clones of projects falling over

--- cookbooks/clone_projects/recipes/default.rb
+++ cookbooks/clone_projects/recipes/default.rb
@@ -18,16 +18,17 @@ orgs = JSON.parse(RestClient.get(organisations_url))
 orgs.each do |org|
   repos_url = "#{github_api_url}/orgs/#{org["login"]}/repos"
   repos = JSON.parse(RestClient.get(repos_url))
-
-  repos.each do |repo|
-    git "#{node[:home_dir]}/projects/#{repo["name"]}" do
-      user node[:user]
-      group node[:user]
-      repository repo["ssh_url"]
-      reference "master"
-      action :sync
+  begin
+    repos.each do |repo|
+      git "#{node[:home_dir]}/projects/#{repo["name"]}" do
+        user node[:user]
+        group node[:user]
+        repository repo["ssh_url"]
+        reference "master"
+        action :sync
+      end
     end
+  rescue
   end
-
 end
 
