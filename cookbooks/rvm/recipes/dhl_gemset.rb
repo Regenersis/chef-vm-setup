48b22e3b437426511b4f2470a713e9a26ce1b074
Colin Gemmell <pythonandchips@gmail.com>
2011-06-26 16:41:43 +0100

    changed rvm gemsets to use tmp folder and templated gem files

--- cookbooks/rvm/recipes/dhl_gemset.rb
+++ cookbooks/rvm/recipes/dhl_gemset.rb
@@ -1,14 +1,22 @@
+["o2_dhl", "o2_dhl_jruby"] do |file|
+  template "#{node[:temp_folder]}/#{file}.gems" do
+    source "#{file}.erb"
+    group node[:user]
+    owner node[:user]
+    mode "0644"
+  end
+end
 
 bash "create dhl gem set" do
   interpreter "bash"
-  cwd "."
+  cwd node[:home_dir]
   user node[:user]
   code <<-EOH
     . #{node[:home_dir]}/.bashrc
     rvm 1.8.7
     rvm gemset create o2_dhl
     rvm 1.8.7@o2_dhl
-    rvm gemset import o2_dhl.gems
+    rvm gemset import #{node[:temp_folder]}/o2_dhl.gems
   EOH
 end
 
@@ -21,6 +29,6 @@ bash "create dhl gem set" do
     rvm jruby
     rvm gemset create o2_dhl_jruby
     rvm jruby@o2_dhl_jruby
-    rvm gemset import o2_dhl_jruby.gems
+    rvm gemset import #{node[:temp_folder]}/o2_dhl_jruby.gems
   EOH
 end
